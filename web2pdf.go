c02e457c93c7b9f114057fcc09c1256aeb094ffe
Alexandre Iooss <erdnaxe@crans.org>
2021-08-13 09:11:14 +0200

    Add more print parameters

--- web2pdf.go
+++ web2pdf.go
@@ -12,23 +12,26 @@ import (
 	"github.com/chromedp/chromedp"
 )
 
-var (
-	printBackground bool
-	scale           float64
-	paperWidth      float64
-	paperHeight     float64
-)
-
 //go:embed cleanup.js
 var cleanupScript string
 
 func main() {
 	// Parse optional command line arguments
 	outputFile := *flag.String("output", "out.pdf", "write PDF document to `path`")
-	printBackground = *flag.Bool("print-background", false, "print background graphics")
-	scale = *flag.Float64("scale", 1, "`scale` of the webpage rendering")
-	paperWidth = *flag.Float64("paper-width", 8.5, "paper `width` in inches, use 8.27 inch for A4")
-	paperHeight = *flag.Float64("paper-height", 11, "paper `height` in inches, use 11.67 for A4")
+	printToPDFParams := page.PrintToPDFParams{}
+	flag.BoolVar(&printToPDFParams.Landscape, "landscape", false, "set paper orientation to landscape")
+	flag.BoolVar(&printToPDFParams.DisplayHeaderFooter, "display-header-footer", false, "display header and footer")
+	flag.BoolVar(&printToPDFParams.PrintBackground, "print-background", false, "print background graphics")
+	flag.Float64Var(&printToPDFParams.Scale, "scale", 1, "`scale` of the webpage rendering")
+	flag.Float64Var(&printToPDFParams.PaperWidth, "paper-width", 8.5, "paper `width` in inches, use 8.27 inch for A4")
+	flag.Float64Var(&printToPDFParams.PaperHeight, "paper-height", 11, "paper `height` in inches, use 11.67 inch for A4")
+	flag.Float64Var(&printToPDFParams.MarginTop, "margin-top", 0.4, "top `margin` in inch")
+	flag.Float64Var(&printToPDFParams.MarginBottom, "margin-bottom", 0.4, "bottom `margin` in inch")
+	flag.Float64Var(&printToPDFParams.MarginLeft, "margin-left", 0.4, "left `margin` in inch")
+	flag.Float64Var(&printToPDFParams.MarginRight, "margin-right", 0.4, "right `margin` in inch")
+	flag.StringVar(&printToPDFParams.PageRanges, "page-ranges", "", "paper `ranges` to print, e.g., '1-5, 8, 11-13' (defaults to all pages)")
+	flag.StringVar(&printToPDFParams.HeaderTemplate, "header-template", "", "HTML template for the print header, use these CSS classes to inject values: date, title, url, pageNumber, totalPages.")
+	flag.StringVar(&printToPDFParams.FooterTemplate, "footer-template", "", "HTML template for the print footer, same format as --header-template")
 	flag.Parse()
 
 	// Parse positional command line arguments
@@ -38,13 +41,25 @@ func main() {
 		log.Fatal("Please specify an URL on the command line")
 	}
 
+	// Print
+	log.Printf("Printing %s to %s", url, outputFile)
+	PrintToPDF(url, printToPDFParams, outputFile)
+}
+
+// PrintToPDF prints the specified `url` to `outputFile` PDF document. Printing
+// options can be specified within `printToPDFParams`.
+func PrintToPDF(
+	url string,
+	printToPDFParams page.PrintToPDFParams,
+	outputFile string,
+) {
 	// Create Chrome DevTool context
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
 	// Capture PDF document
 	var buf []byte
-	if err := chromedp.Run(ctx, printToPDFAction(url, &buf)); err != nil {
+	if err := chromedp.Run(ctx, printToPDFAction(url, printToPDFParams, &buf)); err != nil {
 		log.Fatal(err)
 	}
 
@@ -54,14 +69,19 @@ func main() {
 	}
 }
 
-// printToPDFAction to print a specific page to PDF document in buffer res.
-// It navigates to the specified web page, then inject JS and print.
-func printToPDFAction(url string, res *[]byte) chromedp.Tasks {
+// printToPDFAction is a Chrome DevTool Protocol action that navigates to the
+// specified web page, then inject JS and print it to a PDF document in `res`
+// buffer.
+func printToPDFAction(
+	url string,
+	printToPDFParams page.PrintToPDFParams,
+	res *[]byte,
+) chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(url),
 		chromedp.Evaluate(cleanupScript, nil),
 		chromedp.ActionFunc(func(ctx context.Context) error {
-			buf, _, err := page.PrintToPDF().WithPrintBackground(printBackground).WithScale(scale).WithPaperWidth(paperWidth).WithPaperHeight(paperHeight).Do(ctx)
+			buf, _, err := printToPDFParams.Do(ctx)
 			if err != nil {
 				return err
 			}
